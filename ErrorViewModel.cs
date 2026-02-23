3a077737c59ede8123bee1175592d3e2b2f76f63
Milan <bikineh.work@gmail.com>
2026-02-23 06:23:49 +0100

    Delete ErrorViewModel.cs

--- ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MaschinenDataein.Models.ModelView
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
